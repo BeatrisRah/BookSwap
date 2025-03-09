382431bcdc93d8ca0b65f65d7e55a00c97669d2f
Beatris <beatrisraj7@gmail.com>
2025-03-09 18:15:25 +0200

    Add footer to main

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navigation from './components/Navigation.jsx'
 import Hero from './Hero.jsx'
+import Footer from './components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
     <>
       <Navigation />
-      <Hero />
       <div className="carousel relative container mx-auto" style={{'maxWidth': '1600px'}}>
-
+        <Hero />
+      
       </div>
+      <Footer />
     </>
 )
