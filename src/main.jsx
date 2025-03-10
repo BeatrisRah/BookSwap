cc90d5a429c9cde688512f4df957914fd3f8f2d7
Beatris <beatrisraj7@gmail.com>
2025-03-10 20:46:54 +0200

    Add react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,23 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Navigation from './components/Navigation.jsx'
-import Hero from './Hero.jsx'
-import Footer from './components/Footer.jsx'
+import Navigation from './components/navigation/Navigation.jsx'
+import Hero from './components/hero/Hero.jsx'
+import Footer from './components/footer/Footer.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
     <>
+    <BrowserRouter>
       <Navigation />
       <div className="carousel relative container mx-auto" style={{'maxWidth': '100%'}}>
-        <Hero />
+        <Routes>
+          <Route path='/' element={<Hero />} />
+
+        </Routes>
       
       </div>
       <Footer />
+    </BrowserRouter>
+      
     </>
 )
