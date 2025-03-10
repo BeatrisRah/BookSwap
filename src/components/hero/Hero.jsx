4a614fdfe7ff6011c804ae4f2eda1709309e3827
Beatris <beatrisraj7@gmail.com>
2025-03-10 20:48:43 +0200

    FIx Hero's links

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import ProductList from "../catalog/ProductsSection";
 
 function Hero() {
@@ -13,19 +14,19 @@ function Hero() {
 						<h1 className="text-black font-medium text-4xl md:text-5xl leading-tight mb-2">BookSwap</h1>
 						<p className="font-regular text-gray-900 text-xl mb-8 mt-4">Sell, trade or donate your books!</p>
 						<div className="flex flex-col w-full sm:w-auto sm:flex-row ">
-							<a href="/books"
+							<Link href="/books"
 								className="flex flex-row items-center justify-center w-full px-4 py-4 mb-4 text-sm font-bold bg-blue-300 leading-6 capitalize duration-100 transform rounded-sm shadow cursor-pointer focus:ring-4 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none sm:mb-0 sm:w-auto sm:mr-4 md:pl-8 md:pr-6 xl:pl-12 xl:pr-10   hover:shadow-lg hover:-translate-y-1">
 								Browse Books
 								<span className="ml-4">
 									<svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24" className="w-5 h-5 fill-current"><path fill="currentColor" d="M17.92,11.62a1,1,0,0,0-.21-.33l-5-5a1,1,0,0,0-1.42,1.42L14.59,11H7a1,1,0,0,0,0,2h7.59l-3.3,3.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l5-5a1,1,0,0,0,.21-.33A1,1,0,0,0,17.92,11.62Z"></path>
 									</svg>
 								</span>
-							</a>
+							</Link>
 
-							<a href="/register"
+							<Link to="/register"
 								className="flex items-center justify-center w-full px-4 py-4 text-sm font-bold leading-6 capitalize duration-100 transform border-2 rounded-sm cursor-pointer border-blue-300 focus:ring-4 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none sm:w-auto sm:px-6 border-text  hover:shadow-lg hover:-translate-y-1">
 								Sign up
-							</a>
+							</Link>
 						</div>
 					</div>
 				</div>
