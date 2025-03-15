6c2a7c3800f94f3873ee2f1d63ce108ba1365de5
Beatris <beatrisraj7@gmail.com>
2025-03-15 22:57:48 +0200

    Use hook to fetch data

--- src/components/catalog/catalog-product/Product.jsx
+++ src/components/catalog/catalog-product/Product.jsx
@@ -1,15 +1,17 @@
-export default function Product() {
+import { Link } from "react-router";
+
+export default function Product({book}) {
    return (
     <a className="p-6 max-w-sm border border-indigo-300 rounded-2xl hover:shadow-xl hover:shadow-indigo-50 flex flex-col items-center"
         href="#">
         <img src="https://loremflickr.com/800/600/girl" className="h-3/4 shadow rounded-lg overflow-hidden border" />
         <div className="mt-8 flex flex-col justify-center items-center">
-            <h4 className="font-bold text-xl text-center">BookNAMe</h4>
-            <p>10.99$</p>
+            <h4 className="font-bold text-xl text-center">{book.title}</h4>
+            <p>{book.price}$</p>
             <div className="mt-5">
-                <button type="button" className="inline-flex items-center rounded-md border border-transparent bg-blue-500 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-gray-900">
+                <Link to={`/books/${book.id}`} className="inline-flex items-center rounded-md border border-transparent bg-blue-500 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-gray-900">
                     Details
-                </button>
+                </Link>
             </div>
         </div>
     </a>
