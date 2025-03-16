010958876b2a104d4b9b8bd0d5101ac919e52fdd
Beatris <beatrisraj7@gmail.com>
2025-03-16 21:06:38 +0200

    Logo and icon update

--- src/hooks/useCreateBook.js
+++ src/hooks/useCreateBook.js
@@ -33,11 +33,13 @@ export default function useCreateBook(){
             setPending(true)
             const res = await fetch('https://api.cloudinary.com/v1_1/dserynjly/image/upload', {
                 method:'POST',
-                body:newForm
+                body:newForm,
             })
             const imageData = await res.json()
             const imageUrl = imageData.secure_url;
             
+
+            // TODO: Add owner, available(?) 
             await addDoc(collection(db, 'books'), 
                         {
                             title:data.title,
