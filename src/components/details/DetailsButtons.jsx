e6fd3255473e73b3a011fa79fed64df8f8216d04
Beatris <beatrisraj7@gmail.com>
2025-03-18 18:13:08 +0200

    Buttons on owner, guest and user

---
+++ src/components/details/DetailsButtons.jsx
@@ -0,0 +1,22 @@
+export default function DetailsButtons({isOwner}) {
+    return (
+    <>
+    {isOwner ? 
+        <div className="flex -mx-2 mb-4">
+            <div className="w-1/2 px-2">
+                <button className="w-full bg-green-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-green-800 dark:hover:bg-gray-700">Edit</button>
+            </div>
+            <div className="w-1/2 px-2">
+                <button className="w-full bg-red-800 dark:bg-gray-700 text-gray-100 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-red-700 dark:hover:bg-gray-600">Delete</button>
+            </div>
+        </div>:
+        <div className="flex -mx-2 mb-4">
+            <div className="w-1/2 px-2">
+                <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Trade</button>
+            </div>
+            <div className="w-1/2 px-2">
+                <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">Buy</button>
+            </div>
+        </div>}
+    </>
+    )}
--- src/components/details/ProductDetails.jsx
+++ src/components/details/ProductDetails.jsx
@@ -1,10 +1,14 @@
-import { useParams } from "react-router"
+import { Link, useParams } from "react-router"
 import { useFetchOne } from "../../api/booksApi"
 import NotFound from "../not-found/NotFound"
+import { useAuth } from "../../contexts/AuthContext"
+import DetailsButtons from "./DetailsButtons"
 
 export default function ProductDetails() {
     const {bookId} = useParams()
     const [book, pending, error] = useFetchOne(bookId)
+    const {user} = useAuth()
+    const isOwner = book.owner ===  user?.uid;
 
 
     if (error){
@@ -23,20 +27,22 @@ export default function ProductDetails() {
                     <div className="h-[700px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
                         <img className="w-full h-full object-fit" src={book.imageUrl} alt="Product Image" />
                     </div>
-                    <div className="flex -mx-2 mb-4">
+                    {user ? <DetailsButtons isOwner={isOwner} />:
+                    <div className="flex -mx-2 mb-4 mt-6">
                         <div className="w-1/2 px-2">
-                            <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Add to Cart</button>
+                            <Link 
+                            className="w-full bg-blue-400 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-blue-500 dark:hover:bg-gray-600"
+                            to={'/login'}
+                                >Sigh In To Buy!
+                            </Link>
                         </div>
-                        <div className="w-1/2 px-2">
-                            <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">Add to Wishlist</button>
-                        </div>
-                    </div>
+                    </div>}
+                    
                 </div>
                 <div className="md:flex-1 px-4">
                     <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">{book.title}</h2>
                     <p className="text-gray-600 dark:text-gray-300 text-sm mb-4">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
-                        ante justo. Integer euismod libero id mauris malesuada tincidunt.
+                        {book.author}
                     </p>
                     <div className="flex mb-4">
                         <div className="mr-4">
@@ -44,30 +50,28 @@ export default function ProductDetails() {
                             <span className="text-gray-600 dark:text-gray-300"> {book.price}$</span>
                         </div>
                         <div>
-                            <span className="font-bold text-gray-700 dark:text-gray-300">Condition:</span>
-                            <span className="text-gray-600 dark:text-gray-300"> {book.condition}</span>
+                            <span className="font-bold text-gray-700 dark:text-gray-300">Genre:</span>
+                            <span className="text-gray-600 dark:text-gray-300 capitalize"> {book.genre}</span>
                         </div>
                     </div>
                     
-                    <div className="mb-4">
-                        <span className="font-bold text-gray-700 dark:text-gray-300">Select Size:</span>
-                        <div className="flex items-center mt-2">
-                            <button className="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">S</button>
-                            <button className="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">M</button>
-                            <button className="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">L</button>
-                            <button className="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">XL</button>
-                            <button className="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">XXL</button>
+                    <div className="mb-4 inline-flex justify-center items-center gap-2">
+                        <span className="font-bold text-gray-700 dark:text-gray-300">Condition:</span>
+                        <div className="bg-gray-300 dark:bg-gray-700 capitalize text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2"
+                            >
+                                {book.condition}
                         </div>
                     </div>
-                    <div>
-                        <span className="font-bold text-gray-700 dark:text-gray-300">Product Description:</span>
+                    <div className="mb-4">
+                        <span className="font-bold text-gray-700 dark:text-gray-300">Book Description:</span>
                         <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                            sed ante justo. Integer euismod libero id mauris malesuada tincidunt. Vivamus commodo nulla ut
-                            lorem rhoncus aliquet. Duis dapibus augue vel ipsum pretium, et venenatis sem blandit. Quisque
-                            ut erat vitae nisi ultrices placerat non eget velit. Integer ornare mi sed ipsum lacinia, non
-                            sagittis mauris blandit. Morbi fermentum libero vel nisl suscipit, nec tincidunt mi consectetur.
+                            {book.description}
                         </p>
+                    </div>
+                    <div>
+                        <h3 className="text-gray-500 dark:text-gray-300 text-md font-bold mb-4">
+                            Posted by: {book.ownerEmail}
+                        </h3>
                     </div>
                 </div>
             </div>
