743ad4c98d700e139ea566701a2414ae596a0e65
Beatris <beatrisraj7@gmail.com>
2025-03-19 17:01:37 +0200

    Fix styling

--- src/components/catalog/catalog-product/Product.jsx
+++ src/components/catalog/catalog-product/Product.jsx
@@ -17,24 +17,13 @@ export default function Product({ book }) {
                         <span className="text-3xl font-bold text-slate-900">{book.price}$</span>
                     </p>
                 </div>
-                <button className="flex items-center justify-center rounded-md bg-blue-500 px-2 py-2 text-sm text-white transition hover:bg-gray-700">
-                <svg className="mr-2 h-5 w-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 19V6.2C4 5.0799 4 4.51984 4.21799 4.09202C4.40973 3.71569 4.71569 3.40973 5.09202 3.21799C5.51984 3 6.0799 3 7.2 3H16.8C17.9201 3 18.4802 3 18.908 3.21799C19.2843 3.40973 19.5903 3.71569 19.782 4.09202C20 4.51984 20 5.0799 20 6.2V17H6C4.89543 17 4 17.8954 4 19ZM4 19C4 20.1046 4.89543 21 6 21H20M9 7H15M9 11H15M19 17V21" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
-                    Details
-                </button>
+                <Link to={`/books/${book.id}/details`}>
+                    <button className="flex items-center justify-center rounded-md bg-blue-500 px-2 py-2 text-sm text-white transition hover:bg-gray-700">
+                    <svg className="mr-2 h-5 w-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 19V6.2C4 5.0799 4 4.51984 4.21799 4.09202C4.40973 3.71569 4.71569 3.40973 5.09202 3.21799C5.51984 3 6.0799 3 7.2 3H16.8C17.9201 3 18.4802 3 18.908 3.21799C19.2843 3.40973 19.5903 3.71569 19.782 4.09202C20 4.51984 20 5.0799 20 6.2V17H6C4.89543 17 4 17.8954 4 19ZM4 19C4 20.1046 4.89543 21 6 21H20M9 7H15M9 11H15M19 17V21" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+                        Details
+                    </button>
+                </Link>
             </div>
         </div>
-        // <div className="max-w-xs border border-indigo-300 rounded-2xl hover:shadow-xl hover:shadow-indigo-50 flex flex-col items-center">
-        //     <img src={book.imageUrl} className="h-3/5 w-full shadow rounded-lg object-fill overflow-hidden border" />
-        //     <div className="mt-6 flex flex-col justify-center items-center">
-        //         <h4 className="font-bold text-xl text-center">{book.title}</h4>
-        //         <h5>{book.author}</h5>
-        //         <p className="font-bold text-lg">{book.price}$</p>
-        //         <div className="mt-5">
-        //             <Link to={`/books/${book.id}/details`} className="inline-flex items-center rounded-md border border-transparent bg-blue-500 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-gray-900">
-        //                 Details
-        //             </Link>
-        //         </div>
-        //     </div>
-        // </div>
     );
 }
