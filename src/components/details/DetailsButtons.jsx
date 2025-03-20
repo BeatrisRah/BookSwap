c4a412d9d33c3edba25ecd190c4a8b25969283b4
Beatris <beatrisraj7@gmail.com>
2025-03-20 18:08:47 +0200

    More css fixes

--- src/components/details/DetailsButtons.jsx
+++ src/components/details/DetailsButtons.jsx
@@ -7,17 +7,19 @@ export default function DetailsButtons({isOwner, bookId, openDelete}) {
     <>
     {isOwner ? 
         <div className="flex -mx-2 mb-4">
-            <div className="w-1/2 px-2">
+            <div>
                 <button 
-                className="w-full bg-green-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-green-800 dark:hover:bg-gray-700"
+                className="w-20 text-white py-2 px-4 rounded-full font-bold dark:hover:bg-gray-700"
                 onClick={() => {navigate(`/books/${bookId}/edit`)}}
-                >Edit</button>
+                ><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Complete"> <g id="edit"> <g> <path d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8" fill="none" stroke="#52cb6a" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path> <polygon fill="none" points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8" stroke="#52cb6a" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></polygon> </g> </g> </g> </g></svg></button>
             </div>
-            <div className="w-1/2 px-2">
+            <div>
                 <button 
-                className="w-full bg-red-800 dark:bg-gray-700 text-gray-100 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-red-700 dark:hover:bg-gray-600"
+                className="w-20 text-gray-100 dark:text-white py-2 px-4 rounded-full font-bold"
                 onClick={openDelete}
-                >Delete</button>
+                >
+                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M10 12V17" stroke="#ea3434" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M14 12V17" stroke="#ea3434" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M4 7H20" stroke="#ea3434" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M6 10V18C6 19.6569 7.34315 21 9 21H15C16.6569 21 18 19.6569 18 18V10" stroke="#ea3434" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#ea3434" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+                </button>
                 
             </div>
         </div>:
--- src/components/details/ProductDetails.jsx
+++ src/components/details/ProductDetails.jsx
@@ -7,19 +7,19 @@ import DeleteModal from "./DeleteModal"
 import { useState } from "react"
 
 export default function ProductDetails() {
-    const {bookId} = useParams()
+    const { bookId } = useParams()
     const [book, pending, error, deleteBookHanlder] = useFetchOne(bookId)
-    const {user} = useAuth()
+    const { user } = useAuth()
     const [deleteModalShow, setDelteModalShow] = useState(false)
 
-    const isOwner = book.owner ===  user?.uid;
+    const isOwner = book.owner === user?.uid;
 
 
-    if (error){
+    if (error) {
         return (
-        <>
-            <NotFound />
-        </>)
+            <>
+                <NotFound />
+            </>)
     }
 
     const openDelete = () => {
@@ -30,54 +30,73 @@ export default function ProductDetails() {
         setDelteModalShow(false)
     }
 
-    
+
+
     return (
-    <div className="bg-gray-100 dark:bg-gray-800 py-8">
-        {pending && <div className="loader m-auto"></div>}
-        {deleteModalShow && <DeleteModal closeDelete={closeDelete} onDelete={deleteBookHanlder} />}
-        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex flex-col md:flex-row -mx-4">
-                <div className="md:flex-1 px-4">
-                    <div className="h-[700px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
-                        <img className="w-full h-full object-fit" src={book.imageUrl} alt="Product Image" />
-                    </div>
-                    
-                    
-                </div>
-                <div className="md:flex-1 px-4">
-                    <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">{book.title}</h2>
-                    <p className="text-gray-600 dark:text-gray-300 text-sm mb-4">
-                        {book.author}
-                    </p>
-                    <div className="flex mb-4">
-                        <div className="mr-4">
-                            <span className="font-bold text-gray-700 dark:text-gray-300">Price:</span>
-                            <span className="text-gray-600 dark:text-gray-300"> {book.price}$</span>
+        <div className="bg-gray-100 dark:bg-gray-800 py-8">
+            {pending && <div className="loader m-auto"></div>}
+            {deleteModalShow && <DeleteModal closeDelete={closeDelete} onDelete={deleteBookHanlder} />}
+            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex flex-col md:flex-row -mx-4">
+                    <div className="md:flex-1 px-4">
+                        <div className="h-[700px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
+                            <img className="w-full h-full object-fit rounded-md custom-shadow" src={book.imageUrl} alt="Product Image" />
                         </div>
-                        <div>
-                            <span className="font-bold text-gray-700 dark:text-gray-300">Genre:</span>
-                            <span className="text-gray-600 dark:text-gray-300 capitalize"> {book.genre}</span>
-                        </div>
+
+
                     </div>
-                    
-                    <div className="mb-4 inline-flex justify-center items-center gap-2">
-                        <span className="font-bold text-gray-700 dark:text-gray-300">Condition:</span>
-                        <div className="bg-gray-300 dark:bg-gray-700 capitalize text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2"
-                            >
-                                {book.condition}
-                        </div>
-                    </div>
-                    <div className="mb-4">
-                        <span className="font-bold text-gray-700 dark:text-gray-300">Book Description:</span>
-                        <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">
-                            {book.description}
+                    <div className="md:flex-1 h-1/2 px-4 py-2 bg-white rounded-md">
+
+                        <h2 className="text-4xl font-bold text-gray-800 dark:text-white mt-5 mb-2">{book.title}</h2>
+                        <p className="text-gray-600 dark:text-gray-300 text-md mb-4">
+                            {book.author}
                         </p>
-                    </div>
+                        {/* <div className="flex mb-4">
+                            <div className="mr-4">
+                                <span className="font-bold text-gray-700 dark:text-gray-300">Price:</span>
+                                <span className="text-gray-600 text-xl font-bold dark:text-gray-300"> {book.price}$</span>
+                            </div>
+                            <div>
+                                <span className="font-bold text-gray-700 dark:text-gray-300">Genre:</span>
+                                <span className="text-gray-600 dark:text-gray-300 capitalize"> {book.genre}</span>
+                            </div>
+                        </div> */}
+
+                        <div className="mb-4 flex items-center gap-2">
+                            <div className="inline-flex justify-center items-center gap-2">
+                                <span className="font-bold text-gray-700 dark:text-gray-300">Condition:</span>
+                                <div className="bg-gray-300 dark:bg-gray-700 capitalize text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2"
+                                    >
+                                        {book.condition}
+                                </div>
+                            </div>
+
+                            <span className="font-bold text-gray-700 dark:text-gray-300">Genre:</span>
+                            <span className="text-gray-600 capitalize text-lg"> {book.genre}</span>
+                            
+                        </div>
+
+                        
+                        
+                        <div className="mb-4">
+                            <span className="font-bold text-xl text-gray-700 dark:text-gray-300">Book Description:</span>
+                            <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">
+                                {book.description}
+                            </p>
+                        </div>
+
+                        <div className="mb-4">
+                            <span className="font-bold text-2xl text-gray-700 dark:text-gray-300">Price:</span>
+                            <span className="text-gray-600 text-3xl font-bold dark:text-gray-300"> {book.price}$</span>
+                        </div>
+                    
                     <div>
                         <h3 className="text-gray-500 dark:text-gray-300 text-md font-bold mb-4">
                             Posted by: {book.ownerEmail}
                         </h3>
                     </div>
+
+                    
 
                     {user ? <DetailsButtons isOwner={isOwner} bookId={bookId} openDelete={openDelete} />:
                     <div className="flex -mx-2 mb-4 mt-6">
@@ -89,7 +108,8 @@ export default function ProductDetails() {
                             </Link>
                         </div>
                     </div>}
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>)}
+        </div>)
+}
