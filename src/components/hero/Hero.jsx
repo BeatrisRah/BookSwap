78a88b5e76515ca0817b2045321e19a943d1a981
Beatris <beatrisraj7@gmail.com>
2025-03-20 02:25:31 +0200

    Redesign

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,18 +4,17 @@ import ProductList from "../catalog/ProductsSection";
 function Hero() {
 	return (
 		<>
-			<div className="relative w-full h-[420px] mb-15" id="home">
-				<div className="absolute inset-0 opacity-70">
-					<img src="/book_background.jpg" alt="Background Image" className="object-cover object-center w-full h-full" />
-
+			<div className="relative w-full h-[820px] mb-80" id="home">
+				<div className="absolute inset-0">
+					<img src="/1.png" alt="Background Image" className="object-cover w-full h-full" />
 				</div>
 				<div className="absolute inset-9 flex flex-col md:flex-row items-center justify-between">
 					<div className="ml-1 sm:ml-10 md:ml-36 lg:ml-80 lg-4 md:mb-0 lg:w-1/2">
-						<h1 className="text-black font-medium text-4xl md:text-5xl leading-tight mb-2">BookSwap</h1>
+						<h1 className="text-black font-bold text-4xl md:text-5xl leading-tight mb-2">BookSwap</h1>
 						<p className="font-regular text-gray-900 text-xl mb-8 mt-4">Sell, trade or donate your books!</p>
 						<div className="flex flex-col w-full sm:w-auto sm:flex-row ">
 							<Link to="/books"
-								className="flex flex-row items-center justify-center w-full px-4 py-4 mb-4 text-sm font-bold bg-blue-300 leading-6 capitalize duration-100 transform rounded-sm shadow cursor-pointer focus:ring-4 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none sm:mb-0 sm:w-auto sm:mr-4 md:pl-8 md:pr-6 xl:pl-12 xl:pr-10   hover:shadow-lg hover:-translate-y-1">
+								className="flex flex-row items-center justify-center w-full px-4 py-4 mb-4 text-sm font-bold bg-white leading-6 capitalize duration-100 transform rounded-sm shadow cursor-pointer focus:ring-4 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none sm:mb-0 sm:w-auto sm:mr-4 md:pl-8 md:pr-6 xl:pl-12 xl:pr-10   hover:shadow-lg hover:-translate-y-1">
 								Browse Books
 								<span className="ml-4">
 									<svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24" className="w-5 h-5 fill-current"><path fill="currentColor" d="M17.92,11.62a1,1,0,0,0-.21-.33l-5-5a1,1,0,0,0-1.42,1.42L14.59,11H7a1,1,0,0,0,0,2h7.59l-3.3,3.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l5-5a1,1,0,0,0,.21-.33A1,1,0,0,0,17.92,11.62Z"></path>
@@ -24,15 +23,36 @@ function Hero() {
 							</Link>
 
 							<Link to="/register"
-								className="flex items-center justify-center w-full px-4 py-4 text-sm font-bold leading-6 capitalize duration-100 transform border-2 rounded-sm cursor-pointer border-blue-300 focus:ring-4 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none sm:w-auto sm:px-6 border-text  hover:shadow-lg hover:-translate-y-1">
+								className="flex items-center justify-center w-full px-4 py-4 text-sm font-bold leading-6 capitalize duration-100 transform border-2 rounded-sm cursor-pointer border-white focus:ring-4 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none sm:w-auto sm:px-6 border-text  hover:shadow-lg hover:-translate-y-1">
 								Sign up
 							</Link>
 						</div>
 					</div>
 				</div>
 			</div>
-			<p className="w-11/12 m-auto text-gray-900 text-center p-3 pb-16 border-b-2 border-gray-400 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Find your next great read, declutter your shelves,<br /> or give books a second life—join our community of book lovers today!
+
+			<p className="w-11/12 m-auto text-gray-500 text-center p-3 border-gray-400 lg:mb-8 md:text-lg lg:text-2xl dark:text-gray-400">
+			Find your next great read, declutter your shelves,<br /> 
+			or give books a second life—join our community of book lovers today!
             </p>
+
+			<section className="flex m-auto w-3/5 mt-5 mb-48 items-center">
+					<img src='/ilustration_reading.jpg' className="h-10/12 w-1/2" />
+					<div className="w-1/2 h-10/12 p-20 flex flex-wrap gap-8">
+						<div className="inline-flex items-center gap-2">
+							<svg className="w-12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth={0} /><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" /><g id="SVGRepo_iconCarrier"> <path fillRule="evenodd" clipRule="evenodd" d="M14.364 13.1214C15.2876 14.045 15.4831 15.4211 14.9504 16.5362L16.4853 18.0711C16.8758 18.4616 16.8758 19.0948 16.4853 19.4853C16.0948 19.8758 15.4616 19.8758 15.0711 19.4853L13.5361 17.9504C12.421 18.4831 11.045 18.2876 10.1213 17.364C8.94975 16.1924 8.94975 14.2929 10.1213 13.1214C11.2929 11.9498 13.1924 11.9498 14.364 13.1214ZM12.9497 15.9498C13.3403 15.5593 13.3403 14.9261 12.9497 14.5356C12.5592 14.145 11.9261 14.145 11.5355 14.5356C11.145 14.9261 11.145 15.5593 11.5355 15.9498C11.9261 16.3403 12.5592 16.3403 12.9497 15.9498Z" fill="#60a5fa" /> <path d="M8 5H16V7H8V5Z" fill="#60a5fa" /> <path d="M16 9H8V11H16V9Z" fill="#60a5fa" /> <path fillRule="evenodd" clipRule="evenodd" d="M4 4C4 2.34315 5.34315 1 7 1H17C18.6569 1 20 2.34315 20 4V20C20 21.6569 18.6569 23 17 23H7C5.34315 23 4 21.6569 4 20V4ZM7 3H17C17.5523 3 18 3.44772 18 4V20C18 20.5523 17.5523 21 17 21H7C6.44772 21 6 20.5523 6 20V4C6 3.44772 6.44771 3 7 3Z" fill="#60a5fa" /> </g></svg>
+							<h1 className="text-2xl font-semibold text-blue-400">Browse Books</h1>
+						</div>
+						<div className="inline-flex items-center gap-2">
+							<svg className="w-12" fill="#60a5fa"  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" stroke="#60a5fa" strokeWidth="0.00024000000000000003"><g id="SVGRepo_bgCarrier" strokeWidth={0} /><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" /><g id="SVGRepo_iconCarrier"><path d="M4 21h9.62a3.995 3.995 0 0 0 3.037-1.397l5.102-5.952a1 1 0 0 0-.442-1.6l-1.968-.656a3.043 3.043 0 0 0-2.823.503l-3.185 2.547-.617-1.235A3.98 3.98 0 0 0 9.146 11H4c-1.103 0-2 .897-2 2v6c0 1.103.897 2 2 2zm0-8h5.146c.763 0 1.448.423 1.789 1.105l.447.895H7v2h6.014a.996.996 0 0 0 .442-.11l.003-.001.004-.002h.003l.002-.001h.004l.001-.001c.009.003.003-.001.003-.001.01 0 .002-.001.002-.001h.001l.002-.001.003-.001.002-.001.002-.001.003-.001.002-.001c.003 0 .001-.001.002-.001l.003-.002.002-.001.002-.001.003-.001.002-.001h.001l.002-.001h.001l.002-.001.002-.001c.009-.001.003-.001.003-.001l.002-.001a.915.915 0 0 0 .11-.078l4.146-3.317c.262-.208.623-.273.94-.167l.557.186-4.133 4.823a2.029 2.029 0 0 1-1.52.688H4v-6zM16 2h-.017c-.163.002-1.006.039-1.983.705-.951-.648-1.774-.7-1.968-.704L12.002 2h-.004c-.801 0-1.555.313-2.119.878C9.313 3.445 9 4.198 9 5s.313 1.555.861 2.104l3.414 3.586a1.006 1.006 0 0 0 1.45-.001l3.396-3.568C18.688 6.555 19 5.802 19 5s-.313-1.555-.878-2.121A2.978 2.978 0 0 0 16.002 2H16zm1 3c0 .267-.104.518-.311.725L14 8.55l-2.707-2.843C11.104 5.518 11 5.267 11 5s.104-.518.294-.708A.977.977 0 0 1 11.979 4c.025.001.502.032 1.067.485.081.065.163.139.247.222l.707.707.707-.707c.084-.083.166-.157.247-.222.529-.425.976-.478 1.052-.484a.987.987 0 0 1 .701.292c.189.189.293.44.293.707z" /></g></svg>
+							<h1 className="text-2xl font-semibold text-blue-400">Sell or Donate</h1>
+						</div>
+						<div className="inline-flex items-center gap-2">
+							<svg className="w-12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth={0} /><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" /><g id="SVGRepo_iconCarrier"> <path fillRule="evenodd" clipRule="evenodd" d="M16 6C14.3432 6 13 7.34315 13 9C13 10.6569 14.3432 12 16 12C17.6569 12 19 10.6569 19 9C19 7.34315 17.6569 6 16 6ZM11 9C11 6.23858 13.2386 4 16 4C18.7614 4 21 6.23858 21 9C21 10.3193 20.489 11.5193 19.6542 12.4128C21.4951 13.0124 22.9176 14.1993 23.8264 15.5329C24.1374 15.9893 24.0195 16.6114 23.5631 16.9224C23.1068 17.2334 22.4846 17.1155 22.1736 16.6591C21.1979 15.2273 19.4178 14 17 14C13.166 14 11 17.0742 11 19C11 19.5523 10.5523 20 10 20C9.44773 20 9.00001 19.5523 9.00001 19C9.00001 18.308 9.15848 17.57 9.46082 16.8425C9.38379 16.7931 9.3123 16.7323 9.24889 16.6602C8.42804 15.7262 7.15417 15 5.50001 15C3.84585 15 2.57199 15.7262 1.75114 16.6602C1.38655 17.075 0.754692 17.1157 0.339855 16.7511C-0.0749807 16.3865 -0.115709 15.7547 0.248886 15.3398C0.809035 14.7025 1.51784 14.1364 2.35725 13.7207C1.51989 12.9035 1.00001 11.7625 1.00001 10.5C1.00001 8.01472 3.01473 6 5.50001 6C7.98529 6 10 8.01472 10 10.5C10 11.7625 9.48013 12.9035 8.64278 13.7207C9.36518 14.0785 9.99085 14.5476 10.5083 15.0777C11.152 14.2659 11.9886 13.5382 12.9922 12.9945C11.7822 12.0819 11 10.6323 11 9ZM3.00001 10.5C3.00001 9.11929 4.1193 8 5.50001 8C6.88072 8 8.00001 9.11929 8.00001 10.5C8.00001 11.8807 6.88072 13 5.50001 13C4.1193 13 3.00001 11.8807 3.00001 10.5Z" fill="#60a5fa" /> </g></svg>
+							<h1 className="text-2xl font-semibold text-blue-400">Join the Community</h1>
+						</div>
+					</div>
+			</section>
 
 			
 			<ProductList filter={{latest:true}} />
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -4,8 +4,8 @@ import { useAuth } from "../../contexts/AuthContext";
 export default function Navigation() {
     const {user} = useAuth()
 	return (
-		<nav id="header" className="w-full z-30 top-0 py-1">
-        <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-4 py-3">
+	<nav className="w-full z-30 top-0 py-1">
+        <div className="w-11/12 mx-auto flex flex-wrap items-center justify-between mt-0 px-4 py-3">
 
             <label htmlFor="menu-toggle" className="cursor-pointer md:hidden block">
                 <svg className="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
@@ -17,9 +17,9 @@ export default function Navigation() {
 
             <div className="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1" id="menu">
                 <nav>
-                    <ul className="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
+                    <ul className="md:flex items-center gap-1 justify-between text-base text-gray-700 pt-4 md:pt-0">
                         <li><Link className="inline-block no-underline hover:text-black hover:underline py-2 px-4" to="/books">Books</Link></li>
-                        {user && <li><Link className="inline-block no-underline hover:text-black hover:underline py-2 px-4" to="/books/create">Create Offer</Link></li>}                    
+                        {user && <li><Link className="inline-block border-2 rounded-md border-blue-400 no-underline text-blue-400 hover:text-black hover:underline py-2 px-4" to="/books/create">Create Offer</Link></li>}                    
                     </ul>
                 </nav>
             </div>
