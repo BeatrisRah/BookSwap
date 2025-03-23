d0b5ee818926520625257ca08f11ab7ecab43389
Beatris <beatrisraj7@gmail.com>
2025-03-23 20:04:27 +0200

    Check if user is owner on edit

--- src/components/create-or-update/Edit.jsx
+++ src/components/create-or-update/Edit.jsx
@@ -1,11 +1,17 @@
-import { useParams } from "react-router";
+import { Navigate, useParams } from "react-router";
 import { useEdit, useFetchOne } from "../../api/booksApi";
 import BookForm from "./BookForm";
+import { useAuth } from "../../contexts/AuthContext";
 
 export default function EditProduct() {
+    const {user} = useAuth()
     const {bookId} = useParams()
     const [book, pending, error] = useFetchOne(bookId)
     const [fomrSumbit, handleImageChange, imagePreview] = useEdit(book.imageUrl, bookId)
+
+    if(book.owner !== user.uid){
+        return <Navigate to='/404' />
+    }
 
 
     //error, pending, submitHandler, handleImageChange, book, bookId}
