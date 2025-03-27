c10ff12bc150463a652c39e5e74a1d0b491d1d24
Beatris <beatrisraj7@gmail.com>
2025-03-27 23:11:28 +0200

    Add optional data elements check

--- src/utils/formUtils.js
+++ src/utils/formUtils.js
@@ -1,5 +1,8 @@
-export function checkData(formData){
+export function checkData(formData, optional = []){
     for(const key in formData){
+        if (optional.includes(key)) continue;  
+
+
         if(formData[key] === '') throw new Error('Please fill all inputs!')
     }
 }
