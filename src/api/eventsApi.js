e3811f84dd44b0d2284f58ede928f939ad12c9cb
Beatris <beatrisraj7@gmail.com>
2025-03-27 18:34:48 +0200

    Manage inputs in create event

--- src/api/eventsApi.js
+++ src/api/eventsApi.js
@@ -34,3 +34,52 @@ export function useFetchEvents(){
 
     return [state.pending, state.data]
 }
+const initalState ={
+    title:'',
+    date:'',
+    imageUrl:'',
+    location:'',
+    description:'',
+    additinal:[],
+    
+}
+const reducer = (state, action) => {
+    switch(action.type){
+        case 'SET_TITLE':
+            return{
+                ...state,
+                title: action.data
+            }
+        case 'SET_DATE':
+            return{
+                ...state,
+                date: action.data
+            }
+        case "SET_DESCRIPTION":
+            return { ...state, description: action.data };
+
+        case "SET_LOCATION":
+            return { ...state, location: action.data };
+
+        case "ADD_ITEM":
+            return { ...state, additinal: [...state.additinal, action.data] };
+        case "REMOVE_ITEM":
+            return {
+            ...state,
+            additinal: state.additinal.filter((_, index) => index !== action.data),
+        };
+        case "SET_FILE_URL":
+            return { ...state, fileUrl: action.data };
+        case "RESET":
+            return initalState;
+        default:
+            return state;
+    }
+}
+export function useCreateEvent(){
+    const [state, dispatch] = useReducer(reducer, initalState)
+
+    return[state, dispatch,]
+
+    
+}
--- src/components/events/EventCreate.jsx
+++ src/components/events/EventCreate.jsx
@@ -1,81 +1,164 @@
 import { Navigate } from "react-router";
 import { useAuth } from "../../contexts/AuthContext";
+import { useState } from "react";
+import { useCreateEvent } from "../../api/eventsApi";
+
 
 export default function EventCreate() {
     const { user } = useAuth()
+    const [state, dispatch] = useCreateEvent()
+    const [inputValue, setInputValue] = useState('')
+    
+    const handleDateChange = (e) => {
+        const date = new Date(e.target.value);
+        const formattedDate = `${date.getDate().toString().padStart(2, "0")}/${(date.getMonth() + 1)
+            .toString()
+            .padStart(2, "0")}/${date.getFullYear()}`;
+        dispatch({ type: "SET_DATE", data: formattedDate });
+    };
+
+    
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        dispatch({ type: "SET_FILE", data: file });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // const formData = new FormData(e.currentTarget)
+        // console.log(Object.fromEntries([...formData]));
+        console.log(state);
+        
+    }
 
     if (user.uid !== import.meta.env.VITE_ADMIN_ID) {
         return <Navigate to='/404' />
     }
     return (
-        <div className="flex items-center justify-center">
-            {/* Author: FormBold Team */}
-            <div className="mx-auto w-full max-w-[550px] bg-white">
-                <form className="py-4 px-9">
-                    <div className="mb-5">
-                        <label htmlFor="email" className="mb-3 block text-base font-medium text-[#07074D]">
-                            Send files to this email:
-                        </label>
-                        <input type="email" name="email" id="email" placeholder="example@domain.com" className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+        <div class="mx-auto w-10/12 mt-10 mb-40 border-2 border-blue-400 rounded-lg">
+            <div className="mt-10 text-center text-4xl font-bold">Create Event</div>
+            <form onSubmit={handleSubmit} className="p-8">
+                <div className="flex gap-4">
+                    <div className="w-1/2">
+                        <label
+                        htmlFor="event-title"
+                        className="font-bold"
+                        >Title</label>
+                        <input 
+                        type="Name" 
+                        name="title"
+                        id="event-title"
+                        className="mt-1 block w-full rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" 
+                        placeholder="Event Title" 
+                        onChange={(e) => {
+                            dispatch({ type: "SET_TITLE", data: e.target.value })
+                        }}/>
                     </div>
-                    <div className="mb-6 pt-4">
-                        <label className="mb-5 block text-xl font-semibold text-[#07074D]">
-                            Upload File
-                        </label>
-                        <div className="mb-8">
-                            <input type="file" name="file" id="file" className="sr-only" />
-                            <label htmlFor="file" className="relative flex min-h-[200px] items-center justify-center rounded-md border border-dashed border-[#e0e0e0] p-12 text-center">
-                                <div>
-                                    <span className="mb-2 block text-xl font-semibold text-[#07074D]">
-                                        Drop files here
-                                    </span>
-                                    <span className="mb-2 block text-base font-medium text-[#6B7280]">
-                                        Or
-                                    </span>
-                                    <span className="inline-flex rounded border border-[#e0e0e0] py-2 px-7 text-base font-medium text-[#07074D]">
-                                        Browse
-                                    </span>
-                                </div>
-                            </label>
-                        </div>
-                        <div className="mb-5 rounded-md bg-[#F5F7FB] py-4 px-8">
-                            <div className="flex items-center justify-between">
-                                <span className="truncate pr-3 text-base font-medium text-[#07074D]">
-                                    banner-design.png
-                                </span>
-                                <button className="text-[#07074D]">
-                                    <svg width={10} height={10} viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path fillRule="evenodd" clipRule="evenodd" d="M0.279337 0.279338C0.651787 -0.0931121 1.25565 -0.0931121 1.6281 0.279338L9.72066 8.3719C10.0931 8.74435 10.0931 9.34821 9.72066 9.72066C9.34821 10.0931 8.74435 10.0931 8.3719 9.72066L0.279337 1.6281C-0.0931125 1.25565 -0.0931125 0.651788 0.279337 0.279338Z" fill="currentColor" />
-                                        <path fillRule="evenodd" clipRule="evenodd" d="M0.279337 9.72066C-0.0931125 9.34821 -0.0931125 8.74435 0.279337 8.3719L8.3719 0.279338C8.74435 -0.0931127 9.34821 -0.0931123 9.72066 0.279338C10.0931 0.651787 10.0931 1.25565 9.72066 1.6281L1.6281 9.72066C1.25565 10.0931 0.651787 10.0931 0.279337 9.72066Z" fill="currentColor" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>
-                        <div className="rounded-md bg-[#F5F7FB] py-4 px-8">
-                            <div className="flex items-center justify-between">
-                                <span className="truncate pr-3 text-base font-medium text-[#07074D]">
-                                    banner-design.png
-                                </span>
-                                <button className="text-[#07074D]">
-                                    <svg width={10} height={10} viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path fillRule="evenodd" clipRule="evenodd" d="M0.279337 0.279338C0.651787 -0.0931121 1.25565 -0.0931121 1.6281 0.279338L9.72066 8.3719C10.0931 8.74435 10.0931 9.34821 9.72066 9.72066C9.34821 10.0931 8.74435 10.0931 8.3719 9.72066L0.279337 1.6281C-0.0931125 1.25565 -0.0931125 0.651788 0.279337 0.279338Z" fill="currentColor" />
-                                        <path fillRule="evenodd" clipRule="evenodd" d="M0.279337 9.72066C-0.0931125 9.34821 -0.0931125 8.74435 0.279337 8.3719L8.3719 0.279338C8.74435 -0.0931127 9.34821 -0.0931123 9.72066 0.279338C10.0931 0.651787 10.0931 1.25565 9.72066 1.6281L1.6281 9.72066C1.25565 10.0931 0.651787 10.0931 0.279337 9.72066Z" fill="currentColor" />
-                                    </svg>
-                                </button>
-                            </div>
-                            <div className="relative mt-5 h-[6px] w-full rounded-lg bg-[#E2E5EF]">
-                                <div className="absolute left-0 right-0 h-full w-[75%] rounded-lg bg-[#6A64F1]" />
-                            </div>
-                        </div>
+                    <div className="w-1/2">
+                    <label
+                    htmlFor="event-date"
+                    className="font-bold"
+                    >Date</label>
+                    <input 
+                    id="event-date"
+                    type="date" 
+                    name="date" 
+                    className="mt-1 block w-full rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" 
+                    onChange={handleDateChange}
+                    />
                     </div>
-                    <div>
-                        <button className="hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none">
-                            Send File
+                </div>
+                <div className="my-6 inline-flex gap-4 w-full">
+                        <div className="w-1/2">
+                            <label htmlFor="photobutton" className="font-bold">Event Photo</label>
+                            <div className="relative z-0 mt-0.5 flex w-full -space-x-px">
+                                <input 
+                                name="imageUrl"
+                                id="photobutton" 
+                                type="file" 
+                                accept="image/png, image/jpeg" 
+                                className="block w-full cursor-pointer appearance-none rounded-l-md border border-gray-200 bg-white px-3 py-2 text-sm transition focus:z-10 focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75" 
+                                onChange={handleFileChange}/>
+                            </div>
+                        </div>
+                        <div className="w-1/2">
+                            <label htmlFor="event-location" className="font-bold">Location</label>
+                            <div className="relative z-0 mt-0.5 flex w-full -space-x-px">
+                                <input 
+                                name="location"
+                                id="event-location" 
+                                type="text" 
+                                className="block w-full cursor-pointer appearance-none rounded-l-md border border-gray-200 bg-white px-3 py-2 text-sm transition focus:z-10 focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75"
+                                onChange={(e) => {
+                                    dispatch({ type: "SET_LOCATION", data: e.target.value })
+                                }}
+                                />
+
+                            </div>
+                        </div>
+
+                </div>
+                <div>
+                    <label htmlFor="text" className="font-bold">Description</label>
+                    <textarea 
+                    name="description" 
+                    id="text" 
+                    cols={30} 
+                    rows={10} 
+                    className="mb-10 h-40 w-full resize-none rounded-md border border-slate-300 p-5 font-semibold text-gray-300" 
+                    onChange={(e) =>
+                        dispatch({ type: "SET_DESCRIPTION", data: e.target.value })
+                    } />
+                </div>
+                <div className="w-1/2 p-4 bg-white shadow-md rounded-md">
+                    <h2 className="text-xl font-bold">Additional Details List</h2>
+                    <p className="text-gray-400 italic mb-4">Optinal</p>
+                    <div className="flex gap-2 mb-4">
+                        <input
+                        type="text"
+                        name="additinal"
+                        className="flex-1 p-2 border rounded-md"
+                        placeholder="Enter item..."
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                        />
+                        <button
+                        onClick={() => {
+                            if (inputValue.trim() !== "") {
+                                dispatch({ type: "ADD_ITEM", data: inputValue });
+                                setInputValue("");
+                            }
+                        }}
+                        className="px-4 py-2 bg-blue-500 text-white rounded-md"
+                        type="button">
+                        Add
                         </button>
                     </div>
-                </form>
-            </div>
+
+                    <ul className="list-disc pl-5">
+                    {state.additinal?.map((item, index) => (
+                    <li key={index} className="flex justify-between items-center mb-2">
+                        <span>{item}</span>
+                        <button
+                        type="button"
+                        onClick={() =>
+                            dispatch({ type: "REMOVE_ITEM", data: index })
+                        }
+                        className="px-2 py-1 text-red-500 hover:text-red-700">
+                        ✖
+                        </button>
+                    </li>
+                    ))}
+                    </ul>
+                </div>
+                <div className="text-center mt-10">
+                <button 
+                    className="cursor-pointer rounded-lg bg-blue-700 px-8 py-5 text-sm font-semibold text-white"
+                    type="submit">Create Event</button>
+                </div>
+            </form>
         </div>
+
 
     );
 }
